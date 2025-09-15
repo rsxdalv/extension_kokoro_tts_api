cc6f0e41f3b1322d02537b5a402d6fc0fb8f3b7f
Roberts Slisans <rsxdalv@gmail.com>
2025-09-15 12:13:29 +0300

    add long_description

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 import setuptools
+from pathlib import Path
+
+HERE = Path(__file__).parent
+README = (HERE / "README.md").read_text(encoding="utf-8") if (HERE / "README.md").exists() else ""
 
 setuptools.setup(
     name="extension_openai_tts_api",
@@ -6,6 +10,8 @@ setuptools.setup(
     version="0.13.0",
     author="rsxdalv",
     description="OpenAI compatible TTS API with support for multiple TTS models",
+    long_description=README,
+    long_description_content_type="text/markdown",
     url="https://github.com/rsxdalv/extension_openai_tts_api",
     project_urls={},
     scripts=[],
@@ -23,22 +29,28 @@ setuptools.setup(
     # optional requirements
     extras_require={
         "kokoro": [
-            "extension_kokoro @ git+https://github.com/rsxdalv/extension_kokoro@main",
+            # "extension_kokoro @ git+https://github.com/rsxdalv/extension_kokoro@main",
+            "extension_kokoro", # Use this line if the package is available on PyPI
         ],
         "rvc": [
-            "extension_rvc @ git+https://github.com/rsxdalv/extension_rvc@main",
+            # "extension_rvc @ git+https://github.com/rsxdalv/extension_rvc@main",
+            "extension_rvc", # Use this line if the package is available on PyPI
         ],
         "chatterbox": [
-            "extension_chatterbox @ git+https://github.com/rsxdalv/extension_chatterbox@main",
+            # "extension_chatterbox @ git+https://github.com/rsxdalv/extension_chatterbox@main",
+            "extension_chatterbox", # Use this line if the package is available on PyPI
         ],
         "kitten-tts": [
-            "extension_kitten_tts @ git+https://github.com/rsxdalv/extension_kitten_tts@main",
+            # "extension_kitten_tts @ git+https://github.com/rsxdalv/extension_kitten_tts@main",
+            "extension_kitten_tts", # Use this line if the package is available on PyPI
         ],
         "styletts2": [
-            "extension_styletts2 @ git+https://github.com/rsxdalv/extension_styletts2@main",
+            # "extension_styletts2 @ git+https://github.com/rsxdalv/extension_styletts2@main",
+            "extension_styletts2", # Use this line if the package is available on PyPI
         ],
         "f5-tts": [
-            "extension_f5_tts @ git+https://github.com/rsxdalv/extension_f5_tts@main",
+            # "extension_f5_tts @ git+https://github.com/rsxdalv/extension_f5_tts@main",
+            "extension_f5_tts", # Use this line if the package is available on PyPI
         ],
     },
     classifiers=[
